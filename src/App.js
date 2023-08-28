9b655f22808a19c901dfcedd213bb86af63b1dca
Mr Keltt <66189075+Mr-Keltt@users.noreply.github.com>
2023-08-28 11:51:35 +0000

    Implemented filling in the list of games via an array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from 'react';
 import GameList from "./components/GameList";
 
 function App() {
+    const [games, setGames] = useState([
+        {
+            id: 1,
+            name: 'Name',
+            imgSrc: '',
+            publisher: 'publisher',
+            genres: ['g1', 'g2'],
+            date: '11.11.1111',
+        },
+        {
+            id: 2,
+            name: 'Name',
+            imgSrc: '',
+            publisher: 'publisher',
+            genres: ['g1', 'g2'],
+            date: '11.11.1111',
+        },
+        {
+            id: 3,
+            name: 'Name',
+            imgSrc: '',
+            publisher: 'publisher',
+            genres: ['g1', 'g2'],
+            date: '11.11.1111',
+        },
+    ])
+
     return (
         <div className="App">
-            <GameList></GameList>
+            <GameList games={games}></GameList>
         </div>
     );
 }
