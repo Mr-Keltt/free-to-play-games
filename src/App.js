e929b4343bf636efd00f09cad3ab00e5331a6747
MrKeltt <dmitriydavidenkokeltt@gmail.com>
2023-08-29 20:10:26 +0300

    Restructuring of the router structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { useState } from 'react';
-import {Route, Link, Routes} from "react-router-dom"
+import React, { } from 'react';
+import {Route, Routes, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom"
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import GamePages from "./pages/GamePages";
 import {Layout} from "./components/Layout/Layout";
 
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Layout /> } >
+        <Route index element={<HomePage />} />
+        <Route path="/game/:id" element={<GamePages />} />
+        <Route path="*" element={<NotFoundPage />} />
+    </Route>
+))
+
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<Layout /> } >
-                <Route index element={<HomePage />} />
-                <Route path="/game/:id" element={<GamePages />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Route>
-        </Routes>
+        <RouterProvider router={router} />
     );
 }
 
