05eca4142d4b93676721d75bd8e074c09abea70d
MrKeltt <dmitriydavidenkokeltt@gmail.com>
2023-08-30 01:32:53 +0300

    Implemented downloading information on a specific game via the Free-To-Play Games Database API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { } from 'react';
 import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom"
-import {HomePage, gameLoader} from "./pages/HomePage";
-import NotFoundPage from "./pages/NotFoundPage";
-import GamePages from "./pages/GamePages";
+import {HomePage, gamesLoader} from "./pages/HomePage";
+import {NotFoundPage} from "./pages/NotFoundPage";
+import {gameLoader, GamePages} from "./pages/GamePages";
 import {Layout} from "./components/Layout/Layout";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout /> } >
-        <Route index element={<HomePage />} loader={gameLoader}/>
-        <Route path="/game/:id" element={<GamePages />} />
+        <Route index element={<HomePage />} loader={gamesLoader}/>
+        <Route path="/game/:id" element={<GamePages />} loader={gameLoader}/>
         <Route path="*" element={<NotFoundPage />} />
     </Route>
 ))
--- src/pages/GamePages.jsx
+++ src/pages/GamePages.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import {useLoaderData} from "react-router";
 
-function GamePages(props) {
-    const {id} = useParams();
+function GamePages() {
+    const {game, id} = useLoaderData()
 
     return (
-        <h1>{id}</h1>
+        <>
+            <h1>{game.title}</h1>
+            <img src={game.thumbnail} alt={"thumbnail"}></img>
+        </>
     );
 }
 
-export default GamePages;
+const gameLoader = async ({params}) => {
+    const id = params.id;
+    const url = `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${id}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '906bbbe9d9msh5d6266dfc08d989p1f9678jsn2f4f721153fd',
+            'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const game = await response.json();
+
+        return {game, id}
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export {GamePages, gameLoader};
