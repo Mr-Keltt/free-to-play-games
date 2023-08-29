9a3a911687100c32ffe72599f10af306b66f567d
MrKeltt <dmitriydavidenkokeltt@gmail.com>
2023-08-29 16:49:24 +0300

    Import and initial configuration of react router and react router dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrouserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <BrouserRouter>
+        <App />
+    </BrouserRouter>
 )
